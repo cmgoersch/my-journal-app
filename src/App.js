01da58da1d419a8b8ad7d224a680b50594f74029
Carl-M. Goersch <goersch@madways.de>
2023-04-05 22:52:09 +0200

    design finished

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ function Main() {
       <main className="main">
         <h3 className="main_title">New Entry</h3>
         <Form />
+        <div className="entries_favorites">
+          <h3 className="entries">All Entries</h3>
+          <h3 className="entries_Number">3</h3>
+          <h3 className="favorites">Favorites</h3>
+          <h3 className="favorites_Number">1</h3>
+        </div>
         <Card />
         <Card />
         <Card />
