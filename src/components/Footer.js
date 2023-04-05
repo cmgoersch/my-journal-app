a6abaa4a77477851dc36bbabfe83280254dfadd4
Carl-M. Goersch <goersch@madways.de>
2023-04-05 19:00:42 +0200

    header Footer Button

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <h3>Journal App - 2028</h3>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <>
+      <h2 type="title" className="title">
+        Journal
+      </h2>
+    </>
+  );
+}
